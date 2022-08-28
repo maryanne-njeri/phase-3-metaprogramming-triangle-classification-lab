625bc8ed6093345480549b67f33da3f6426e3265
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-08-28 04:53:50 +0300

    commit tree e39f90a28a772525c5048404194a538002eb1920

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+
+  attr_accessor :length_one, :length_two, :length_three
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    if is_triangle?
+      triangle_sides = [length_one, length_two, length_three]
+      if triangle_sides.uniq.size == 1
+        :equilateral
+      elsif triangle_sides.uniq.size == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+  private
+
+  def is_triangle?
+    sort_sides = [length_one, length_two, length_three].sort
+    longest_side = sort_sides.pop
+    longest_side < sort_sides.sum
+  end
 end
